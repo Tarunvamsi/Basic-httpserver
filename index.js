e633d8cd53517a4c949c661365b6d5abcba90595
Vamsi <pusarlatarunvamsi@gmail.com>
2023-12-02 16:07:55 +0530

    Updated with Queryparam , method handling(get,put,post,delete)

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 var express = require("express");
-const res = require("express/lib/response");
 var app = express();
-const port = 3000
+const port = 3000;
 
-
-function calsum(counter){
-    var sum=0;
-    for(var i=0;i<counter;i++){
-        sum = sum+i;
+function calsum(counter) {
+    var sum = 0;
+    for (var i = 0; i < counter; i++) {
+        sum = sum + i;
     }
-    return sum 
+    return sum;
 }
 
-
-function handleFirstRequest(req, res){
-    var counter = req.query.counter;   //to pass the input as QeuryParam(localhost:3000/route?counter=1000)
+function handleFirstRequest(req, res) {
+    var counter = req.query.counter; //to pass the input as QueryParam(localhost:3000/handlesum?counter=1000)
     var caltdsum = calsum(counter);
-    console.log(caltdsum)
+    console.log(caltdsum);
     var answer = "the sum is " + caltdsum;
     res.send(answer);
 }
-app.get('/handlesum', handleFirstRequest)
 
-function started(){
-    console.log(`Examle app listening on port ${port}`)    
+function handlepost(req, res) {
+    res.send("Hello, World!");
 }
-app.listen(port,started)     
 
+function handlePut(req, res) {
+    res.send("This is a PUT request!");
+}
 
+function handleDelete(req, res) {
+    res.send("This is a DELETE request!");
+}
 
+app.get('/handlesum', handleFirstRequest);
+app.post('/handlepost', handlepost);
+app.put('/handleput', handlePut);
+app.delete('/handledelete', handleDelete);
+
+function started() {
+    console.log(`Example app listening on port ${port}`);
+}
+
+app.listen(port, started);
